7c5a8a9659b1a97d9edfc9f7674998c8129e6868
Zhao Shenyang <dev@zsy.im>
2024-07-17 23:07:50 +0800

    minor fix

--- service.py
+++ service.py
@@ -6,8 +6,9 @@ import bentoml
 from bentoml.validators import DataframeSchema
 
 
-MODEL_ID = "microsoft/resnet-50"
 MODEL_SIZE = "large"  # {"small", "base", "large"}
+MODEL_ID = f"Salesforce/moirai-1.0-R-{MODEL_SIZE}"
+
 PRED_LEN = 20  # prediction length
 CTX_LEN = 200  # context length
 PATCH_SIZE = "auto"
@@ -32,7 +33,7 @@ class Moirai:
         from uni2ts.model.moirai import MoiraiForecast, MoiraiModule
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model = MoiraiForecast(
-            module=MoiraiModule.from_pretrained(f"Salesforce/moirai-1.0-R-{MODEL_SIZE}"),
+            module=MoiraiModule.from_pretrained(MODEL_ID),
             prediction_length=PRED_LEN,
             context_length=CTX_LEN,
             patch_size=PATCH_SIZE,
